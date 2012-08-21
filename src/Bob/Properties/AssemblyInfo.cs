10da137f1d1449fc9c408831de5685faf7862ac4
Aaron Powell <me@aaron-powell.com>
2012-08-21 22:38:05 +1000

    assembly versioning

--- src/Bob/Properties/AssemblyInfo.cs
+++ src/Bob/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bob")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bob wasn't a great product for Microsoft but maybe Bob can add what's missing for Windows 8")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bob")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: ComVisible(false)]
