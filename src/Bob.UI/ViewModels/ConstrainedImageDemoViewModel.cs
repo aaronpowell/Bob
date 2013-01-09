0bcaf4efdab59ec352911d7d7fe055c49f8affb5
Daniel Chambers <daniel@digitallycreated.net>
2013-01-09 11:21:45 +1100

    Fixed page navigation on two demo views

--- src/Bob.UI/ViewModels/ConstrainedImageDemoViewModel.cs
+++ src/Bob.UI/ViewModels/ConstrainedImageDemoViewModel.cs
@@ -1,10 +1,28 @@
-﻿using Okra.Navigation;
+﻿using System.ComponentModel;
+using System.Composition;
+using Okra.Core;
+using Okra.Navigation;
+using PropertyChanged;
 
 namespace Bob.UI.ViewModels
 {
     [ViewModelExport(typeof(ConstrainedImageDemoViewModel))]
-    public class ConstrainedImageDemoViewModel
+    public class ConstrainedImageDemoViewModel : INotifyPropertyChanged
     {
-         
+        private readonly INavigationManager _navigationManager;
+
+        [ImportingConstructor]
+        public ConstrainedImageDemoViewModel(INavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+            GoBackCommand = new DelegateCommand(navigationManager.GoBack);
+        }
+
+        [DoNotNotify]
+        public DelegateCommand GoBackCommand { get; set; }
+
+        public bool CanGoBack { get { return _navigationManager.CanGoBack; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- src/Bob.UI/ViewModels/DayMonthYearSelectorDemoViewModel.cs
+++ src/Bob.UI/ViewModels/DayMonthYearSelectorDemoViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Composition;
+using Okra.Core;
 using Okra.Navigation;
+using PropertyChanged;
 
 namespace Bob.UI.ViewModels
 {
     [ViewModelExport(typeof(DayMonthYearSelectorDemoViewModel))]
     public class DayMonthYearSelectorDemoViewModel : INotifyPropertyChanged
     {
-        public DayMonthYearSelectorDemoViewModel()
+        private readonly INavigationManager _navigationManager;
+
+        [ImportingConstructor]
+        public DayMonthYearSelectorDemoViewModel(INavigationManager navigationManager)
         {
+            _navigationManager = navigationManager;
             MinDate = new DateTime(1988, 6, 12);
             MaxDate = new DateTime(2020, 5, 25);
             SortYearsAscending = true;
+            GoBackCommand = new DelegateCommand(navigationManager.GoBack);
         }
 
         public DateTime? Date { get; set; }
@@ -21,6 +29,11 @@ namespace Bob.UI.ViewModels
         public DateTime MaxDate { get; set; }
 
         public bool SortYearsAscending { get; set; }
+
+        [DoNotNotify]
+        public DelegateCommand GoBackCommand { get; set; }
+
+        public bool CanGoBack { get { return _navigationManager.CanGoBack; } }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
